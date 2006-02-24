5b5df1986dc7e4afa1f7d4383913ca11e0b2b046
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-02-24 11:02:58 +0000

    Made the JavaScript object oriented changed the id="" attributes of telescope and roof to be unique Gave the roof status a little randomness cuz that's what the roof does (and you can see AJAX at work.)
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@7 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/roof.js
+++ scripts/roof.js
@@ -1,52 +1,58 @@
-var roof_base="/roof/"
+var Roof = {
 
-function roof_command(methodName) {
-    var error = getElement("error");
-    
-    replaceChildNodes(error);
-    roof_setStatus("Waiting for response...");
-    
-    deferred = doSimpleXMLHttpRequest(roof_base+methodName);
-    deferred.addCallback(function (res) {
-        response = res.responseText;
-        // todo: explicitly check for python exception message
-        if(response.indexOf("Traceback") == -1) {
-            roof_setStatus("Success");
-        }
-        else {
-            roof_setStatus("Python error");
-            replaceChildNodes(error, SPAN(null, response));
-        }
-    });
-    deferred.addErrback(function (err) {
-        roof_setStatus("Error: " + repr(err));
-    });
-}
+    'base' : '/roof/',
 
-function roof_setStatus(statusText) {
-    replaceChildNodes(getElement("status"), SPAN(null, statusText));
-}
-
-function updateProgress() {
-    log("Updating");
-    // get status
-    deferred = loadJSONDoc(roof_base+"webPosition");
-    deferred.addCallback(function(progress) {
-        log("Progress: " + progress);
-        // update roof position pct text
-        replaceChildNodes(getElement("roofpospct"), 
-                          SPAN(null, (progress * 100) + "%"));
+    command: function(methodName) {
+        var error = getElement("roof_error");
         
-        // update progress bar
-        bar = getElement("progress");
-        bar.style.width = (progress * 300) + "px";
+        replaceChildNodes(error);
+        Roof.setStatus("Waiting for response...");
+        
+        deferred = doSimpleXMLHttpRequest(Roof.base+methodName);
+        deferred.addCallback(function (res) {
+            response = res.responseText;
+            // todo: explicitly check for python exception message
+            if(response.indexOf("Traceback") == -1) {
+                Roof.setStatus("Success");
+            }
+            else {
+                Roof.setStatus("Python error");
+                replaceChildNodes(error, SPAN(null, response));
+            }
+        });
+        deferred.addErrback(function (err) {
+            Roof.setStatus("Error: " + repr(err));
+        });
+    },
 
-        // do it again in 5 seconds
-        callLater(2, updateProgress);
-    });
-    deferred.addErrback(function(err) {
-        roof_setStatus("Error updating position: " + repr(err));
-        // wait longer
-        callLater(10, updateProgress);
-    });
-}
+    setStatus: function(statusText) {
+        replaceChildNodes(getElement("roof_status"), SPAN(null, statusText));
+    },
+
+    updateProgress: function() {
+        log("Updating");
+        // get status
+        deferred = loadJSONDoc(Roof.base+"webPosition");
+        deferred.addCallback(function(progress) {
+            log("Progress: " + progress);
+            // update roof position pct text
+            replaceChildNodes(getElement("roofpospct"), 
+                              SPAN(null, (progress * 100).toPrecision(3) + "%"));
+            
+            // update progress bar
+            bar = getElement("roof_progress");
+            bar.style.width = (progress * 300).toFixed(0) + "px";
+
+            // do it again in 5 seconds
+            callLater(2, Roof.updateProgress);
+        });
+        deferred.addErrback(function(err) {
+            Roof.setStatus("Error updating position: " + repr(err));
+            // wait longer
+            callLater(10, Roof.updateProgress);
+        });
+    },
+
+};
+
+window.addEventListener('load', Roof.updateProgress, false);
--- scripts/telescope.js
+++ scripts/telescope.js
@@ -1,57 +1,60 @@
-var telescope_base="/telescope/";
+var Telescope = {
+    'base' : '/telescope/',
 
-function telescope_command(methodName) {
-    setError("");
-    
-    deferred = doSimpleXMLHttpRequest(telescope_base+methodName);
-    deferred.addCallback(function (res) {
-        response = res.responseText;
-        if(response.indexOf("Traceback") != -1) {
-            setError("Python error: \n" + response);
-        }
-    });
-    deferred.addErrback(function (err) {
-        setError("Error: " + repr(err));
-    });
-}
+    command: function(methodName) {
+        Telescope.setError("");
+        
+        deferred = doSimpleXMLHttpRequest(Telescope.base+methodName);
+        deferred.addCallback(function (res) {
+            response = res.responseText;
+            if(response.indexOf("Traceback") != -1) {
+                Telescope.setError("Python error: \n" + response);
+            }
+        });
+        deferred.addErrback(function (err) {
+            Telescope.setError("Error: " + repr(err));
+        });
+    },
 
-function telescope_setStatus(statusText) {
-    replaceChildNodes(getElement("status"), SPAN(null, statusText));
-}
+    setStatus: function(statusText) {
+        replaceChildNodes(getElement("telescope_status"), SPAN(null, statusText));
+    },
 
-function setError(errorText) {
-    replaceChildNodes(getElement("error"), SPAN(null, errorText));
-}
+    setError: function(errorText) {
+        replaceChildNodes(getElement("telescope_error"), SPAN(null, errorText));
+    },
 
-function move() {
-    // send move command
-    log("webPosition" + queryString(getElement("theform")));
-    deferred = doSimpleXMLHttpRequest(telescope_base+"webMove?" + 
-                                      queryString(getElement("theform")));
-    deferred.addCallback(function (res) {
-        response = res.responseText;
-        if(response.indexOf("Traceback") != -1) {
-            setError("Python error: \n" + response);
-        }
-    });
-    deferred.addErrback(function (err) {
-        setError("Error: " + repr(err));
-    });
-    // TODO: clean up this duplicate code
-}
+    move: function() {
+        // send move command
+        log("webPosition" + queryString(getElement("telescope_form")));
+        deferred = doSimpleXMLHttpRequest(Telescope.base+"webMove?" + 
+                                          queryString(getElement("telescope_form")));
+        deferred.addCallback(function (res) {
+            response = res.responseText;
+            if(response.indexOf("Traceback") != -1) {
+                Telescope.setError("Python error: \n" + response);
+            }
+        });
+        deferred.addErrback(function (err) {
+            Telescope.setError("Error: " + repr(err));
+        });
+        // TODO: clean up this duplicate code
+    },
 
-function telescope_updateStatus() {
-    log("Updating status");
-    // get status
-    deferred = doSimpleXMLHttpRequest(telescope_base+"statusSummary");
-    deferred.addCallback(function(req) {
-        telescope_setStatus(req.responseText);
-        // do it again in 5 seconds
-        callLater(2, updateStatus);
-    });
-    deferred.addErrback(function(err) {
-        telescope_setStatus("Error updating status: " + repr(err));
-        // wait longer
-        callLater(10, updateStatus);
-    });
-}
+    updateStatus: function() {
+        log("Updating status");
+        // get status
+        deferred = doSimpleXMLHttpRequest(Telescope.base+"statusSummary");
+        deferred.addCallback(function(req) {
+            Telescope.setStatus(req.responseText);
+            // do it again in 5 seconds
+            callLater(2, Telescope.updateStatus);
+        });
+        deferred.addErrback(function(err) {
+            Telescope.setStatus("Error updating status: " + repr(err));
+            // wait longer
+            callLater(10, Telescope.updateStatus);
+        });
+    }
+
+};
