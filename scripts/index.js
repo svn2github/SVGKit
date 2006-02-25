81c92da91935445d5e4bdbac51e637e0b18bc0e7
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-02-25 22:59:07 +0000

    added four new panel stubs (UI Debugging, UI Logger, Javascript Interpreter, Python Interpreter). started working on UI Logger, but it's not fulling working yet.
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@21 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,30 @@
 function topTimeFunc() {       
-        topTimeRefresh = window.setTimeout( "topTimeFunc()", 1000 );       
-        var dateToday = new Date();       
-        $('top_time').innerHTML= toISOTimestamp(dateToday)}
+   topTimeRefresh = window.setTimeout( "topTimeFunc()", 1000 );       
+   var dateToday = new Date();       
+   $('top_time').innerHTML= toISOTimestamp(dateToday)}
 
 function togglePanel(elem,source,sourcetype) {
-  makePanel(elem);
-  toggleMenuItem(elem)}
+   makePanel(elem);
+   toggleMenuItem(elem)}
 
 function toggleMenuItem(elem) {
-  toggleElementClass("panelShown","bar_" + elem)}
+   toggleElementClass("panelShown","bar_" + elem)}
 
 function panelIsVisible(elem) {
-  return !hasElementClass(elem, "panelInvisible")}
+   return !hasElementClass(elem, "panelInvisible")}
 
 function makePanel(elem) {
-  toggleElementClass("panelInvisible", elem)}
+   toggleElementClass("panelInvisible", elem)}
 
 function roundPanelCorners() {
-    roundClass("div", "panel", {bgColor: "#000000"})}
+   roundClass("div", "panel", {bgColor: "#000000"})}
 
+function panelMochiLogger(inline) {
+   var m = MochiKit.LoggingPane;
+   m._loggingPane = new m.LoggingPane(inline, MochiKit.Logging.logger);
+   return m._loggingPane}
 
 function onLoadScripts() {
-    addLoadEvent(roundPanelCorners());
-    addLoadEvent(topTimeFunc())}
+   addLoadEvent(roundPanelCorners());
+   addLoadEvent(topTimeFunc());
+   }
