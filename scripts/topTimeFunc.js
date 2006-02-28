d0a1ec1f6cc7d28a468f08bf8588a2c632b98ae0
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-02-28 16:02:31 +0000

    added close panel link on the top of each panel
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@26 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/topTimeFunc.js
+++
@@ -1,5 +0,0 @@
-function topTimeFunc() {       
-        topTimeRefresh = window.setTimeout( "topTimeFunc()", 1000 );       
-        var dateToday = new Date();       
-        $('top_time').innerHTML= toISOTimestamp(dateToday);    
-        }
