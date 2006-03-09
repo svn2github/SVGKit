5ed014a2a167f9f88598dbfe1e183fa9f1d32f15
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-03-09 16:16:03 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@44 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/programChips.js
+++ scripts/programChips.js
@@ -4,7 +4,7 @@ var ProgramChips = {
     idbase: 'expt_',
 
     command: function(methodName) {
-        this.setError("No error so far");
+        //this.setError("No error so far");
         log("ProgramChips.command(" + methodName +")");
         deferred = doSimpleXMLHttpRequest(this.base+methodName);
         deferred.addCallback(function (res) {
@@ -18,16 +18,17 @@ var ProgramChips = {
         });
     },
 
-    setDAC: function(vref) {
+    setPNDAC: function(vref) {
+        log("got to setPNDAC, vref=" +vref);
         if (vref == 'all') {
-            for (i = 0; i <= 6; i++) {
-                ProgramChips.setDAC(i);
-            }
-            ProgramChips.setDAC('bias');
+//            for (i = 0; i <= 6; i++) {
+//                ProgramChips.setPNDAC(i);
+//            }
+//            ProgramChips.setPNDAC('bias');
         }
         else {
-            query_vref = queryString(getElement(idbase+'PulseNetDACForm'+vref));
-            ProgramChips.command('programDACPulseNet?vref='+vref+'&'+query_vref);         
+//            query_vref = queryString(getElement(idbase+'PulseNetDACForm'+vref));
+//            ProgramChips.command('programDACPulseNet?vref='+vref+'&'+query_vref);         
         }
     },
     
