ec77f51f6c91be6b293b23e1314dd4c5bda8b461
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-03-10 14:14:34 +0000

    added SETI Programming panel and related javascript
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@47 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

---
+++ scripts/setiProgram.js
@@ -0,0 +1,73 @@
+var setiProgram = {
+
+    base:   '/expt/',
+    idbase: 'seti_program_',
+    expt:   'ExperimentState.pulsenetSETI_State',
+    
+    command: function(methodName) {
+        log("setiProgram.command(" + methodName +")");
+        deferred = doSimpleXMLHttpRequest(this.base+methodName);
+        deferred.addCallback(function (res) {
+            response = res.responseText;
+            if(response.indexOf("Traceback") != -1) {
+                this.setError("Python error: \n" + response);
+            }
+        });
+        deferred.addErrback(function (err) {
+            this.setError("Error: " + repr(err));
+        });
+    },
+
+    checkAll: function(state,boxname) {
+        for (i = 0; i < 32; i++) {
+            getElement(document.forms[this.idbase].elements['PN'+i+boxname]).checked = state;
+        }
+    },
+    
+    setCoincMask: function() {
+        for (i = 0; i < 8; i++) {
+            checked = []; checked_cap = [];
+            for (j = 0; j < 4; j++) {
+                k = 4*i+j;
+                checked[j] = getElement(document.forms[this.idbase].elements['PN'+k+'_coincMask']).checked;
+                if (checked[j] == true) {checked_cap[j] = '1';}
+                else                    {checked_cap[j] = '0';}
+            }
+            setiProgram.command('setCoincMask?uCno='+i+
+                                '&pulseNet0='+checked_cap[0]+'&pulseNet1='+checked_cap[1]+
+                                '&pulseNet2='+checked_cap[2]+'&pulseNet3='+checked_cap[3]); 
+        }
+    },
+    
+    program: function (option) {
+        for (i = 7; i >= 0; i--) {
+            i_reverse = 7-i;
+            for (j = 0; j < 4; j++) {
+                k = 4*i_reverse + j
+                if (getElement(document.forms[this.idbase].elements['PN'+k+'_program']).checked == true) {
+                    if (option == 'reset') {
+                        setiProgram.command('resetASIC?uCno=' + i + '&asicno=' + j);
+                    }
+                    else if (option == 'program') {
+                        thresh    = getElement(document.forms[this.idbase].elements['thresh']).value;
+                        veto      = getElement(document.forms[this.idbase].elements['veto']).value;
+                        clockhalf = getElement(document.forms[this.idbase].elements['clockhalf']).value;
+                        setiProgram.command('setiConfigASIC?uCno=' + i + '&asicno=' + j + 
+                                            '&threshold=' + thresh + '&veto=' + veto + '&halfclock=' + clockhalf);
+                    }
+                }
+            }
+        }
+    },
+   
+    setStatus: function(statusText) {
+        replaceChildNodes(getElement(this.idbase + "status"), 
+                          SPAN(null, statusText));
+    },
+
+    setError: function(errorText) {
+        replaceChildNodes(getElement(this.idbase + "error"), 
+                          SPAN(null, errorText));
+    },
+
+};
