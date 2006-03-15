4de50c14b1dd0a63b25c8e0d39d05d1ba5da26e4
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-03-15 12:46:40 +0000

    added 2nd observatory svg
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@55 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/observatory.js
+++ scripts/observatory.js
@@ -5,19 +5,24 @@ var obsSvg = {
     tele_base: '/telescope/',
     tele_idbase: 'telespope_',
     obsSvg1: [],
+    obsSvg2: [],
 
     init: function () {
         obsSvg.obsSvg1 = document.getElementById("obsSvg1").contentDocument ;
+        obsSvg.obsSvg2 = document.getElementById("obsSvg2").contentDocument ;
     },
 
     translateRoof: function (percentOpen) {
-        var roof = document.getElementById("obsSvg1").contentDocument.getElementById("roof");
-        roof.setAttribute("transform", 'translate(' + 290*(1-percentOpen/100) + ',0)' );
+        var roof1 = document.getElementById("obsSvg1").contentDocument.getElementById("roof");
+        var roof2 = document.getElementById("obsSvg2").contentDocument.getElementById("roof");
+        roof1.setAttribute("transform", 'translate(' + 290*(1-percentOpen/100) + ',0)' );
+        roof2.setAttribute("transform", 'translate(' + 290*(1-percentOpen/100) + ',0)' );
     },
     
     rotateTelescope: function (degrees) {
-        var telescope = document.getElementById("obsSvg1").contentDocument.getElementById("telescope");
-        var transform = 'translate(535,210) rotate(' + degrees + ') translate(-535,-210)';
+        var telescope1 = document.getElementById("obsSvg1").contentDocument.getElementById("telescope");
+        var telescope2 = document.getElementById("obsSvg2").contentDocument.getElementById("telescope");
+        var transform  = 'translate(535,210) rotate(' + degrees + ') translate(-535,-210)';
         telescope.setAttribute("transform", transform);
     },
 
