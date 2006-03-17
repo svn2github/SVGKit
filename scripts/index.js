799ad6f73189655af6987b9c9ad55a38674ff89b
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-03-17 16:02:50 +0000

    added support for cookies.  now, panels left open will be opened the next time the browser loads the page
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@60 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,89 @@
-function topTimeFunc() {       
-    topTimeRefresh = window.setTimeout( "topTimeFunc()", 1000 );       
-    var dateToday = new Date();       
-    $('top_time').innerHTML= toISOTimestamp(dateToday)}
-
-function togglePanel(elem,source,sourcetype) {
-    makePanel(elem);
-    toggleMenuItem(elem)}
-
-function toggleMenuItem(elem) {
-    toggleElementClass("panelShown","bar_" + elem)}
-
-function panelIsVisible(elem) {
-    return !hasElementClass(elem, "panelInvisible")}
-
-function makePanel(elem) {
-    toggleElementClass("panelInvisible", elem)}
-
 function roundPanelCorners() {
-    roundClass("div", "panel", {bgColor: "#000000"})}
+    roundClass("div", "panel", {bgColor: "#000000"});
+}
 
 function loadClearSkyClock() {
     var dateToday = new Date();
     var myImg = IMG( {'src':'http://cleardarksky.com/csk/getcsk.php?id=OkRdgObMA'} );
     replaceChildNodes('img_skyclock', myImg);
-    $('timestamp_skyclock').innerHTML= toISOTimestamp(dateToday);}
-    
+    $('timestamp_skyclock').innerHTML= toISOTimestamp(dateToday);
+}
+
+function topTimeFunc() {       
+    topTimeRefresh = window.setTimeout( "topTimeFunc()", 1000 );       
+    var dateToday = new Date();       
+    $('top_time').innerHTML= toISOTimestamp(dateToday);
+}
+
+function togglePanel(elem,source,sourcetype) {
+    makePanel(elem);
+    toggleMenuItem(elem);
+}
+
+function toggleMenuItem(elem) {
+    toggleElementClass("panelShown","bar_" + elem);
+    //set cookies for panel state
+    if (hasElementClass("bar_" + elem, "panelShown")) {
+        Cookie.create(Cookie.panelPrefix + elem, "true", 1);
+    }
+    else Cookie.erase(Cookie.panelPrefix + elem);
+}
+
+function panelIsVisible(elem) {
+    return !hasElementClass(elem, "panelInvisible");
+}
+
+function makePanel(elem) {
+    toggleElementClass("panelInvisible", elem);
+}
+
+var Cookie = {
+    panelPrefix: 'panel_',
+
+    create: function (name,value,days) {
+    	if (days) {
+    		var date = new Date();
+    		date.setTime(date.getTime()+(days*24*60*60*1000));
+    		var expires = "; expires="+date.toGMTString();
+    	}
+    	else var expires = "";
+    	document.cookie = name + "=" + value + expires + "; path=/";
+    },
+
+    read: function (name) {
+    	var nameEQ = name + "=";
+    	var ca = document.cookie.split(';');
+    	for(var i=0;i < ca.length;i++) {
+    		var c = ca[i];
+    		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    	}
+    	return null;
+    },
+
+    erase: function (name) {
+    	Cookie.create(name,"",-1);
+    },    
+
+    restorePanels: function () {
+    	var cookieArray = document.cookie.split(';');
+    	for(var i=0;i < cookieArray.length;i++) {
+    		while (cookieArray[i].charAt(0)==' ') {
+                cookieArray[i] = cookieArray[i].substring(1, cookieArray[i].length);
+            }
+            var pair = cookieArray[i].split('=');
+            if (pair[0].substring(0, Cookie.panelPrefix.length) == Cookie.panelPrefix) {
+                if (pair[1] == "true") {
+                    togglePanel(pair[0].substring(Cookie.panelPrefix.length, pair[0].length));
+                }
+            }
+        }
+    },
+
+}
+
 addLoadEvent(roundPanelCorners);
 addLoadEvent(topTimeFunc);
-addLoadEvent(jsInterpreterManager.initialize); 
+addLoadEvent(jsInterpreterManager.initialize);
+addLoadEvent(Cookie.restorePanels); 
 addLoadEvent(State.updateWithHash);
