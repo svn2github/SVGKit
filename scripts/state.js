ebb997c10bce8db6cb7888968ec51307721d3fa6
showard <showard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-03-20 04:18:14 +0000

    -modfied cameras to check for /dev/video0, and if not found, return dummy  image paths. -implemented fancy sqlobject state system in osetistate -wrote beginning of event system in osetievent.py - needs to be modified to use  InheritableSQLObject -modified osetiexptctrl.py and osetipower.py a bit to use new state/event stuff -changed state getTree method to make a hierarchical dictionary, and modified  state.js to read the new hierarchical object -slight modification to status.kid to make DAC voltages a list
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@71 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/state.js
+++ scripts/state.js
@@ -2,33 +2,65 @@ var State = {
     base: "/state/",
     idbase: "state_",
     stateHash: null,
-    currentState: [],
+    currentState: null,
 
     makeHash: function () {
-        //makeHash is function that constructs State.stateHash - a hash table with 
-        //with (key, pointer) pairs where: 
+        // makeHash is function that constructs State.stateHash - a hash table
+        // with (key, pointer) pairs where: 
         //   key = a state variable (e.g. "ExperimentState.pmt_DAC_State[6].voltage")
         //   pointer = a pointer to one HTML element that matches that state variable
         //So, for example, 
         // > stateHash[key] = pointer
         // > stateHash[ExperimentState.pmt_DAC_State[6].voltage] = [object HTMLSpanElement]
-        log("Getting state from server.");
-        State.stateHash = [];
-        deferred = loadJSONDoc(State.base + "webGetState");
-        deferred.addCallback(function(result) {
-            log("State received.  Creating hash table.");
-            var resultkeys = keys(result);
-            forEach(getElementsByTagAndClassName('span','autoUpdate'),
-                function(elem) {                
-                    stateVar = elem.className.match(/ExperimentState[..a-zA-Z0-9\_\[\]]*/)[0];
-                    State.stateHash[stateVar] = elem;
-                    }
-            )
-            log("Finished creating State.stateHash.  It has " + keys(State.stateHash).length + " entries.");
-        });
-        deferred.addErrback(function (err) {
-            log("Error updating state: " + repr(err));
-        });
+        State.stateHash = {};
+        forEach(getElementsByTagAndClassName('span','autoUpdate'),
+            function(elem) {                
+                var stateVar = elem.className.match(/ExperimentState[..a-zA-Z0-9\_\[\]]*/)[0];
+                if(typeof(State.stateHash[stateVar]) == 'undefined' ||
+                   State.stateHash[stateVar] == null) {
+                    State.stateHash[stateVar] = [];
+                }
+                State.stateHash[stateVar].push(elem);
+                }
+        )
+        log("Finished creating State.stateHash.  It has " + 
+            keys(State.stateHash).length + " entries.");
+    },
+    
+    processState: function (obj, oldObj, path) {
+        if(typeof(obj) == "object") {
+            if(typeof(obj.length) == "undefined" || obj.length == null) {
+                // object
+                for(var key in obj) {
+                    var newpath = path + '.' + key;
+                    var val = obj[key];
+                    if(typeof(val) == "undefined" || val == null)
+                        log("State error on " + newpath);
+                    var oldval = (oldObj == null ? null : oldObj[key]);
+                    State.processState(val, oldval, newpath);
+                }
+            }
+            else {
+                // list
+                for(var i = 0; i < obj.length; i++) {
+                    var newpath = path + "[" + i + "]";
+                    var oldval = (oldObj == null ? null : oldObj[i]);
+                    State.processState(obj[i], oldval, newpath);
+                }
+            }
+        }
+        else {
+            // primitive
+            elts = State.stateHash[path];
+            if(typeof(elts) == "undefined" || elts == null) {
+                //log("Unused state: " + path + " (" + obj + ")");
+            }
+            else {
+                for(var i = 0; i < elts.length; i++) {
+                    elts[i].innerHTML = obj;
+                }
+            }
+        }
     },
 
     updateWithHash: function () {
@@ -37,16 +69,9 @@ var State = {
             log("Getting new hash.");
             State.makeHash();
         }
-        deferred = loadJSONDoc(State.base + "webGetState");
+        var deferred = loadJSONDoc(State.base + "webGetState");
         deferred.addCallback(function (newState) {
-            var hashkeys = keys(State.stateHash);
-            forEach(hashkeys, 
-                function (hashkey) {
-                    if (newState[hashkey] != State.currentState[hashkey]) {
-                        State.stateHash[hashkey].innerHTML = newState[hashkey];
-                    }
-                }
-            )
+            State.processState(newState, State.currentState, 'ExperimentState');
             State.currentState = newState;
             callLater(30, State.updateWithHash);
         });
@@ -54,5 +79,5 @@ var State = {
             log("Error updating state: " + repr(err));
             callLater(30, State.updateWithHash);
         });
-    },  
+    },
 };
