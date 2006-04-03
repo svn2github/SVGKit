1f07c6e6325a2b1664c7d5530ee9ee5ff23e5b59
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-04-03 23:58:21 +0000

    tidied things up
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@150 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/index.js
+++ scripts/index.js
@@ -94,11 +94,15 @@ var Cookie = {
     read: function (name) {
     	var nameEQ = name + "=";
     	var ca = document.cookie.split(';');
-    	for(var i=0;i < ca.length;i++) {
+    	for(var i=0; i<ca.length; i++) {
     		var c = ca[i];
-    		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    	}
+    		while (c.charAt(0)==' ') {
+                c = c.substring(1, c.length);
+            }
+    		if (c.indexOf(nameEQ) == 0) { 
+                return c.substring(nameEQ.length, c.length);
+            }
+        }
     	return null;
     },
 
@@ -113,7 +117,6 @@ var Cookie = {
                 cookieArray[i] = cookieArray[i].substring(1, cookieArray[i].length);
             }
             var pair = cookieArray[i].split('=');
-            //log(pair[0] + " = " + pair[1]);
             if (pair[0].substring(0, Cookie.panelPrefix.length) == Cookie.panelPrefix) {
                 if (pair[1] == "true") {
                     togglePanel(pair[0].substring(Cookie.panelPrefix.length, pair[0].length));
