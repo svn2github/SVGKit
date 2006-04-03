fd50184b3388991689992b158c59bb0774976f00
howard <howard@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2006-04-03 02:48:51 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@143 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- scripts/setiEvent.js
+++ scripts/setiEvent.js
@@ -3,39 +3,25 @@ var setiEvent = {
     idbase: "seti_event_",
     rows:    4,
     columns: 8,
-    plotWidth:  80,
-    plotHeight: 60,
+    plotWidth:  600,
+    plotHeight: 100,
     
     clearView: function () {
         //plotTableElem = $(setiEvent.idbase+'plot_table');
-        for (j=0; j<8; j++) {
-            for (i=0; i<8; i++){
-                currentCell = $(setiEvent.idbase+'plot_'+i+"_"+j);
+        for (i=0; i<8; i++) {
+                currentCell = $(setiEvent.idbase+'plot_'+i);
                 currentCell.innerHTML = "";
-            }
         }
     },
     
     setView: function() {
-        setiEvent.columns = getElement(document.forms[this.idbase].elements['set_columns']).value
-        setiEvent.rows    = getElement(document.forms[this.idbase].elements['set_rows']).value
         setiEvent.clearView();
-        if      (setiEvent.columns == 1) {setiEvent.plotWidth = 640}
-        else if (setiEvent.columns == 2) {setiEvent.plotWidth = 320}
-        else if (setiEvent.columns == 4) {setiEvent.plotWidth = 160}
-        else if (setiEvent.columns == 8) {setiEvent.plotWidth = 80}        
+        setiEvent.rows = getElement(document.forms[this.idbase].elements['set_rows']).value
         setiEvent.plotHeight = setiEvent.plotWidth * 3/4;
-        var blankImg = IMG( {'src':'/static/images/blank_'+
-                                    setiEvent.plotWidth+'x'+
-                                    setiEvent.plotHeight+'.png'} );
-        for (j=0; j<setiEvent.rows; j++) {
-            for (i=0; i<setiEvent.columns; i++){
-                var blankImg = IMG( {'src':'/static/images/blank_'+
-                                            setiEvent.plotWidth+'x'+
-                                            setiEvent.plotHeight+'.png'} );
-                currentCell = $(setiEvent.idbase+'plot_'+i+"_"+j);
-                currentCell.appendChild(blankImg);
-            }
+        for (i=0; i<setiEvent.rows; i++) {
+            var blankImg = IMG( {'src':'/static/images/blank_600x100.png'} );
+            currentCell = $(setiEvent.idbase+'plot_'+i);
+            currentCell.appendChild(blankImg);
         }
     },
 };
