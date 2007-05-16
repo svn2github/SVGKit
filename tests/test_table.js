3ab00d482ecf14cb2dab577d6fad81d0499b8757
jasonphysics <jasonphysics@d1cfad4a-9815-0410-8b6e-f025c34e1c2d>
2007-05-16 02:35:19 +0000

    Conversions now open in a new window. target="_blank" is a deprecated feature, but very useful since you can't right click on the submit button to choose if you want to open it in a new window, and going back is SLOW
    git-svn-id: svn://svn.code.sf.net/p/svgkit/code/trunk@354 d1cfad4a-9815-0410-8b6e-f025c34e1c2d

--- tests/test_table.js
+++ tests/test_table.js
@@ -1,3 +1,5 @@
+converter_url = 'http://brainflux.org/cgi-bin/convert_svg.py'
+
 function setupTopBar() {
     getElement('start').value = 0
     var i = 0;
@@ -64,36 +66,61 @@ function addTests(start, end) {
         if (i>=start && i<=end) {
             log("doing test number ", i, "name ", test);
             var code = (''+testFunctions[test]+'\n').replace(/ +/g, " ");
-            var functionArea, svgSrcArea, button, svgTD, button;
+            var functionArea, svgSrcArea, button, svgTD, button, form;
             var canvasTD = null
             var tr = TR(null, TD(null, ""+i+": "+test, BR(null), IMG({'src':'canvas_tests_images/'+test+'.png'}) ) );
             var include_canvas = (type=='canvas')
             if (include_canvas)
                 appendChildNodes(tr, canvasTD=TD({width:210}) )
+            var types = ['svg', 'pdf', 'png', 'jpg']
             appendChildNodes(tr, 
-                              TD(null, functionArea=TEXTAREA({'rows':"14", 'cols':"40", 'wrap':"off"}, code),
+                              TD(null, functionArea=TEXTAREA({'rows':"14", 'cols':"40", 'wrap':"off", id:test+'_code'}, code),
                                        BR(null),
                                        buttonDoIt=INPUT({type:"button", value:"Do It"}),
                                        " ",
                                        buttonXML=INPUT({type:"button", value:"Update XML"}) ),
                               svgTD=TD({width:210}),
-                              TD(null, 
-                                FORM( { name:'form_'+test, method:'post', action:"http://brainflux.org/cgi-bin/convert_svg.py"}, 
-                                   svgSrcArea=TEXTAREA({rows:"14", cols:"60", wrap:"off", name:'source'} ,"SVG Source"),
-                                   BR(null),
-                                   buttonSVG=INPUT({type:"submit", name:"type", value:"svg"}), " ",
-                                   buttonPDF=INPUT({type:"submit", name:"type", value:"pdf"}), " ",
-                                   buttonPNG=INPUT({type:"submit", name:"type", value:"png"}), " ",
-                                   buttonJPEG=INPUT({type:"submit", name:"type", value:"jpg"}) )
+                              TD(null,  
+                                // Form will open result in new window. 
+                                // target="_blank" is a deprecated feature, but very useful since you can't right click 
+                                // on the submit button to choose if you want to open it in a new window, and going back is SLOW
+                                form=FORM( { name:'form_'+test, method:'post', action:converter_url, id:test+'_form', target:"_blank"}, 
+                                   svgSrcArea=TEXTAREA({rows:"14", cols:"60", wrap:"off", name:'source', id:test+'_src'} ,"SVG Source"),
+                                   BR(null) // Buttons get added below.
                                 )
+                              )
                         );
             appendChildNodes(tbody, tr);
+            var make_input = function(type) {
+                var button=INPUT({type:"submit", name:"type", value:type, id:test+'_'+type})
+                //addToCallStack(button, 'onclick', partial(convert, test, type))  // Doesn't work to external site
+                return SPAN(null, button, " ")  // Put a space after each button
+            }
+            appendChildNodes(form, map(make_input, types))
+            appendChildNodes(form, '(opens in new window)')
             addToCallStack(buttonDoIt, 'onclick', partial(doTest,canvasTD, svgTD, functionArea, svgSrcArea, buttonXML, include_canvas) );
             doTest(canvasTD, svgTD, functionArea, svgSrcArea, buttonXML, include_canvas);
         }
         i++;
     }
 }
+
+/*
+// For security reasons, browsers won't let JavaScript get the converted file - it must be opened in a new window.
+function convert(name, type) {
+    var src = name+'_src'
+    var contents = formContents(name+'_form')
+    //var s = queryString(contents)
+    var d = doXHR(converter_url, {method: 'POST', queryString: contents})
+    var got_converted = function(response) {
+        log(repr(response))
+    }
+    var failed_converted = function(response) {
+        log("failed: "+repr(response))
+    }
+    d.addCallbacks(got_converted, failed_converted)
+}
+*/
 
 type = 'svg'  // Can be overriden by other files.
 addLoadEvent(setupTopBar)
